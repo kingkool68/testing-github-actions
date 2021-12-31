5c8bcba814e0f75fc9d8f7ee2d3881be94cbacdd
Russell Heimlich <kingkool68@gmail.com>
2021-12-30 23:00:56 -0500

    Initial commit of caching dependencies

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>GitHub Actions Cache Test</title>
+	<link rel="stylesheet" href="assets/css/style.min.css" type="text/css" media="all">
+</head>
+
+<body>
+	<h1>Hello World!</h1>
+	<p>Seeing how caching dependencies works.</p>
+</body>
+</html>
